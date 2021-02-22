7c09d8b7f154b7d8ed7927bcedc24899b619443f
michaelcurry <curry51@purdue.edu>
2021-02-22 12:13:31 -0500

    HW 4 and 5... I think

--- leap_year.py
+++ leap_year.py
@@ -1,23 +1,23 @@
+################################################################################
+# Author: Michael Curry
+# Date: 02/21/2021
+# This program sees if a year 
+# is a leap year.
+################################################################################
+# Input Year
 year = int(input('Please input a year: ')) # Year variable
-four = 4  # Variable for dividend 4
-hundred = 100  # Variable for dividend 100
-fhundred = 400  # Variable for dividend 400
-
-checkune = year % 4  # First condition
-checkdeux = year % 100  # Second condition
-checktrois = year % 400  # Third condition
-
-if checkune == 0 and checkdeux == 0 and checktrois == 0:
+# Calculations
+if year % 4 == 0 and year % 100 == 0 and year % 400 == 0: # First Conditonal Check
      print(f'In the year {year}, there are 29 days in February.')
-elif checkune == 0 and checkdeux != 0:
+elif year % 4 == 0 and year % 100 != 0: # Second Conditonal Check
      print(f'In the year {year}, there are 29 days in February.')
-elif (checkune and checkdeux) == 0 and checktrois != 0:
+elif (year % 4 and year % 100) == 0 and year % 400 != 0: # Third Conditonal Check
      print(f'In the year {year}, there are 28 days in February.')
-elif checkune == 0 and (checkdeux and checktrois != 0):
+elif year % 4 == 0 and (year % 100 and year % 400 != 0): # Fourth Conditonal Check
      print(f'In the year {year}, there are 28 days in February.')
-elif checkune != 0:
+elif year % 4 != 0: # Fifth Conditonal Check
      print(f'In the year {year}, there are 28 days in February.')
-elif checktrois != 0:
+elif year % 400 != 0: # Sixth Conditonal Check
      print(f'In the year {year}, there are 28 days in February.')
-else:
+else: # Final
      print(f'In the year {year}, there are 28 days in February.')
