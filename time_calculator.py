7292952881cac5d576a67aa727e205654e6c6f7d
michaelcurry <curry51@purdue.edu>
2021-02-22 20:17:10 -0500

    HW 7

--- time_calculator.py
+++ time_calculator.py
@@ -9,9 +9,32 @@ days = inp // 86400 # Days Calculation
 hours = inp % (24 * 3600) // 3600 # Hours Calculation
 minutes = inp % 3600 // 60 # Minutes Calculation
 seconds = ((inp % (24 * 3600)) % 3600) % 60 # Days Calculation
-if inp < 60: print('  The number of seconds is less than one minute.') # Conditional Check 1
-elif inp in range(60,3601): print(f' {minutes} minute(s).') if inp % 60 == 0 else print(f' {minutes} minute(s) and {seconds} second(s)') # Conditional Check 2
-elif inp in range(3601, 86400): print(f' {hours} hours(s).') if inp % 3600 == 0 else print(f'  {hours} hours(s) and {seconds} second(s)') # Conditional Check 3
-elif inp % 86400 == 0: print(f'  {days} day(s).') # Conditional Check 4
-else: print(f'  {days} days(s) and {hours} hour(s) and {minutes} minute(s) and {seconds} second(s)') # Conditional Check 5
-
+if days:
+    print(f'  {inp} seconds is: ', end='')
+    print(f'{days} day(s)', end ='')
+    if hours:
+        print(' and ', end ='')
+        print(f'{hours} hours(s)', end ='')
+    if minutes:
+        print(' and ', end ='')
+        print(f'{minutes} minutes(s)', end ='')
+    if seconds:
+        print(' and ', end ='')
+        print(f'{seconds} seconds(s)', end ='')
+elif hours:
+    print(f'  {inp} seconds is: ', end='')
+    print(f'{hours} hours(s)', end ='')
+    if minutes:
+        print(' and ', end ='')
+        print(f'{minutes} minute(s)', end ='')
+    if seconds:
+        print(' and ', end='')
+        print(f'{seconds} second(s)', end ='')
+elif minutes:
+    print(f'  {inp} seconds is: ', end='')
+    if seconds:
+        print(' and ', end ='')
+    print(f'{minutes} minute(s)', end ='')
+else:
+    print('  The number of seconds is less than one minute', end='')
+print('.')
